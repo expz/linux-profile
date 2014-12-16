514c602dcb1901f048ea0648e9899801c0873364
Jonathan Skowera <jskowera@gmail.com>
2014-12-16 17:19:22 -0500

    removed bundle dir from .vim

--- .vim/compile-options.sh
+++ .vim/compile-options.sh
@@ -1,12 +1,31 @@
-# copy this file to the vim source directory and run
-# then execute
-#   $ make
-#   $ sudo make install
+#!/bin/sh
+#
+# How to compile vim with all desired options
+#
+# Install prerequisites
+#
+#   sudo apt-get install lua5.1 liblua5.1-0 liblua5.1-0-dev perl libperl5.18 libperl-dev python2.7 libpython2.7 libpython2.7-dev python3.4 libpython3.4 libpython3.4-dev ruby2.1 libruby2.1 ruby2.1-dev
+#
+# Install vim source
+#
+#   sudo apt-get install mercurial
+#   cd ~/opt
+#   hg clone https://vim.googlecode.com/hg/ vim
+#   cd vim
+#   hg pull
+#   hg update
+#   cd src
+#   make distclean  # if you built vim before
+#
+# copy this file to the vim source directory and run:
+#
+#   cp ~/.vim/compile-options.sh .
+#   chmod a+x compile-options.sh
+#   ./compile-options.sh
+#   make
+#   sudo make install
 
-# hangul input is not required to input hangul -- it is a workaround when other input methods don't work
-
-# requires
-# sudo apt-get install lua5.1 liblua5.1-0 liblua5.1-0-dev perl libperl5.18 libperl-dev python2.7 libpython2.7 libpython2.7-dev python3.4 libpython3.4 libpython3.4-dev ruby2.1 libruby2.1 ruby2.1-dev
+# hangul input is not desirable -- it is a workaround when other input methods don't work
 
 
 ./configure \
