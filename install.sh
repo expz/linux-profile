061ee0aea23ee98a162d4ca419766d2922603983
Jonathan Skowera <jskowera@gmail.com>
2015-10-30 18:36:49 -0400

    major changes to install.sh; fixed bug plus added protection against overwrites, etc

--- install.sh
+++ install.sh
@@ -1,31 +1,93 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Prerequisite
+# This file should be in a *recursive* clone of the git repo:
 # 
-#   cd ~
-#   git clone --recursive https://expz@gitlab.com/expz/linux-profile .
+#   git clone --recursive https://github.com/expz/linux-profile.git
 #
-# This script finishes installing vim after this recursive clone
+# This script installs the linux profile into the current user's home directory.
 #
 
 ROOT_DIR="${0%/*}"
+HOME_DIR=$(cd ~; pwd)
 
-# create bin and tmp directories
-mkdir -p ~/bin
-mkdir -p ~/tmp
+# cp -b --suffix didn't work with directories, so make this function
+function save_original() {
+  if [ -e "$1" ]; then
+    mv "$1" "$1"-replaced-$(date +%Y-%m-%d-%H-%M-%S)
+  fi
+}
 
-# copy bin and tmp 
-cp -R "$ROOT_DIR"/bin/* ~/bin/
-cp -R "$ROOT_DIR"/tmp/* ~/tmp/
+# could make rm -i confirm deletion of *every* file, 
+# but not just top-level folders
+function delete_originals() {
+  for fn in $(find "$1" -maxdepth 1 -name '*-replaced-*'); do
+    read -p "delete '$fn' (y/n)? " -n 1 yn; echo
+    case "$yn" in
+      y|Y ) rm -rf "$fn";;
+      * ) ;;
+    esac
+  done
+}
 
-# copy vim
-cp -R "$ROOT_DIR"/.vim ~
-cp -R "$ROOT_DIR"/.vundle ~
-cp "$ROOT_DIR"/.vimrc ~
+function safe_copy() {
+  save_original "$2"/"$1"
+  cp -R "$ROOT_DIR"/"$1" "$2"
+}
 
-# copy bash profile, saving existing profile first
-cp ~/.bashrc ~/.bashrc.old 
-cp "$ROOT_DIR"/.bashrc ~
 
-# install plugins using vundle
-vim +BundleInstall +qall
+INSTALL=true
+CLEAN=false
+
+case "$1" in
+  '--clean')
+    CLEAN=true
+    ;;
+  '--only-clean')
+    INSTALL=false
+    CLEAN=true
+    ;;
+  '--help')
+    INSTALL=false
+    CLEAN=false
+    printf "installs profile and saves existing files to backups copies in the same
+directory with the suffix '-replaced-[DATE]'.
+
+If there are errors, make sure you executed a recursive clone of the git repo:
+
+  git clone --recursive https://github.com/expz/linux-profile.git
+  (or retroactively: git submodule update --init --recursive)
+
+usage: install.sh [--clean|--only-clean|--help]
+
+--clean       install and delete backup copies
+--only-clean  only delete backup copies (confirm each deletion)
+--help        display this message
+"
+    exit 0 
+    ;;
+esac
+
+if "$INSTALL"; then
+  # create bin and tmp directories
+  mkdir -p ~/bin
+  mkdir -p ~/tmp
+
+  # copy bin and tmp without overwriting
+  cp -nR "$ROOT_DIR"/bin/* ~/bin/
+  cp -nR "$ROOT_DIR"/tmp/* ~/tmp/
+
+  # copy vim plugins and settings
+  safe_copy '.vim' "$HOME_DIR"
+  safe_copy '.vundle' "$HOME_DIR"
+  safe_copy '.vimrc' "$HOME_DIR"
+
+  # copy bashrc
+  safe_copy '.bashrc' "$HOME_DIR"
+
+  # install plugins using vundle
+  vim +BundleInstall +qall
+fi
+
+if "$CLEAN"; then
+  delete_originals "$HOME_DIR"
+fi
